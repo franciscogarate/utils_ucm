91bccf0477c1d0dd72fb382f8635c53155b4f847
franciscogarate <fgaratesantiago@gmail.com>
2025-01-08 10:38:42 +0100

    actualiza datos

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ from .utils import *
 from .actuarial_funciones import *
 from .version import version
 from .decesos_funciones import *
+from .tablas import *
---
+++ tablas.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+censo = pd.read_excel('data/censo.xlsx')
+
+print(censo)
