ac0e1bad5d9f84e4b96c89c748e23b93e3f1ea6a
Francisco Gárate <franciscogarate@users.noreply.github.com>
2025-03-15 10:32:33 +0100

    Update clr.py

--- clr.py
+++ clr.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-file = ('./data/EIOPA_RFR_20241231_Term_Structures.xlsx)
+file = ('./utils_ucm/data/EIOPA_RFR_20241231_Term_Structures.xlsx')
 rfr = pd.read_excel(file, sheet_name='RFR_spot_no_VA', skiprows=9, usecols=(1,2), names=['t','Euro'])
 clr = rfr['Euro'].loc[:150].tolist()
